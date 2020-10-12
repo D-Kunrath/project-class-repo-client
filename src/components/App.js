5fc18e2f035c3238bd0a7e0d8082c1ca874c15a5
Rubem Diogo <rubemdiogo@gmail.com>
2020-10-12 14:22:25 -0400

    home

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
+import 'bootstrap/dist/css/bootstrap.css'
+import Home from "../route-components/Home"
 function App() {
   return (
     <div className="App">
     <BrowserRouter>
       <Switch>
-        <Route to='/' component='' />
+        <Route to='/' component={Home} />
       </Switch>
     </BrowserRouter>
       
