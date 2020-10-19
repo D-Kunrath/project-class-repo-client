65bd417e2acedf43357a07069a341cb964ece431
D-Kunrath <dkunrath@gmail.com>
2020-10-19 17:46:19 -0300

    ready for deployment

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  // baseURL: "http://localhost:4000/api",
+  baseURL: process.env.REACT_APP_API,
 });
 
 const loggedInUser = localStorage.getItem("loggedInUser");
